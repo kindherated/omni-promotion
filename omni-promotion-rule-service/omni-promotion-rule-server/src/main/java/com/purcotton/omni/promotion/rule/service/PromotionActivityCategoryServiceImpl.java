3e17a2abc82082bdf17c55c58f9c69ed11520a23
ouxiaoqiong <kindherate@163.com>
2020-07-14 20:00:47 +0800

    增加日志

--- omni-promotion-rule-service/omni-promotion-rule-server/src/main/java/com/purcotton/omni/promotion/rule/service/PromotionActivityCategoryServiceImpl.java
+++ omni-promotion-rule-service/omni-promotion-rule-server/src/main/java/com/purcotton/omni/promotion/rule/service/PromotionActivityCategoryServiceImpl.java
@@ -1,7 +1,9 @@
 package com.purcotton.omni.promotion.rule.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class PromotionActivityCategoryServiceImpl implements PromotionActivityCategoryService {
 }
